0e7f2eb8a30959f18268c17f458cccd73da93ac0
msantosh <mar.dls@tutamail.com>
2020-06-04 18:41:42 -0500

    error making query

--- reporte1HEB.py
+++ reporte1HEB.py
@@ -1,18 +1,19 @@
 #reporte 1 HEB
 
 from azure.cosmos import CosmosClient, PartitionKey, exceptions
-
+import json
 import os
-url = os.environ['ACCOUNT_URI']
-key = os.environ['ACCOUNT_KEY']
+
+url = 'https://cosmos-arpiheb-dev.documents.azure.com:443/' #os.environ['ACCOUNT_URI']
+key = 'xzvXRlcvgHViT47OJQEq2ylbwePcjf6ALJQRCYEg43yOFrnoPvIXwR4LKXZmTrZxGgiOJ4YenSPIGy4wRmgvhg=='
 client = CosmosClient(url, credential=key)
+
+database_name = 'cosmos-arpiheb-dev'#"testDatabase"
+container_name = 'arpi'#"testDatabase"
 
 database = client.get_database_client(database_name)
 container = database.get_container_client(container_name)
 
 # Enumerate the returned items
-import json
-for item in container.query_items(
-        query='SELECT * FROM mycontainer r WHERE r.id="item3"',
-        enable_cross_partition_query=True):
+for item in container.query_items(query='SELECT * FROM c'):
     print(json.dumps(item, indent=True))
